727efa62d640cd6aefa3075eb8549e4ea40df55a
boluwatifee1 <boluwatifee4@gmail.com>
2022-07-22 01:34:30 +0100

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+// =========> Question <==========
+// Given a string return a new string with the reversed order of the original string.
+// Example:
+// reverseString('hello') === 'olleh'
+// reverseString('Howdy') === 'ydwoH'
+
+const reverseString = (str) => {
+    return console.log(str.split("").reverse().join(""))
+}
+
+reverseString("hello");
